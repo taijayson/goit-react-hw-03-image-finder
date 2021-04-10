0d2ebb0cb6224cdf28cb997a3544c7df9ed80205
Pavel Denisov <denisov.p.p@gmail.com>
2021-04-10 20:06:31 +0300

    Updates

---
+++ static/js/main.acbb927f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),r=n(3),s=n.n(r),a=n(0),o=function(){return Object(a.jsx)("div",{className:"App",children:"Hello"})};s.a.render(Object(a.jsx)(i.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.acbb927f.chunk.js.map
